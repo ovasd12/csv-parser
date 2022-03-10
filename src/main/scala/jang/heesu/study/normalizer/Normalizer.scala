c0fa73c811810df785c373436ec5c9c6a60683a5
taeyoung kim <ty@miribit.co.kr>
2022-03-10 22:38:05 +0900

    대강 수정해야할 사항2

---
+++ src/main/scala/jang/heesu/study/normalizer/Normalizer.scala
@@ -0,0 +1,12 @@
+package jang.heesu.study.normalizer
+
+import jang.heesu.study.normalizer.dictionary.NormalizeDictionary
+
+//TODO 실제 Dictionary matching 통하여 normalize 실행할 클래스
+class Normalizer(dictionary:NormalizeDictionary) {
+
+  def normalize(raw:String): String ={
+      ???
+  }
+
+}
