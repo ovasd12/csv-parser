144264f3fef413904730f3c8b60a555d813f4858
jang <ovasd12@gmail.com>
2022-03-29 19:56:53 +0900

    scalatest 로 변경

--- src/test/scala/jang/heesu/study/parser/ParserTest.scala
+++ src/test/scala/jang/heesu/study/parser/ParserTest.scala
@@ -1,45 +1,43 @@
 package jang.heesu.study.parser
 import jang.heesu.study.csv.CsvInfo
 import org.junit.Assert.assertEquals
+import org.scalatest.FunSuite
 
 import scala.io.Source
 
-class ParserTest {
-  @org.junit.Test
-  def testRemoveWarapper_1() ={
+class ParserTest extends FunSuite{
+  test("An empty Set should have size 0") {
+    assert(Set.empty.size == 0)
+  }
+  test("testRemoveWarapper_1"){
     val wrapper:String = "\""
     val testString = "\"test\""
     val right : String = "test"
-    assertEquals(Parser.removeWrapper(testString,wrapper), right)
+    assert(right == Parser.removeWrapper(testString,wrapper))
   }
-  @org.junit.Test
-  def testRemoveWarapper_2() ={
+  test("testRemoveWarapper_2"){
     val wrapper:String = "\""
     val testString = "\"te\"st\""
     val right : String = "te\"st"
-//    assertEquals( right, Parser.removeWrapper(testString,wrapper))
-    assertEquals(Parser.removeWrapper(testString,wrapper), right)
+    assert(right == Parser.removeWrapper(testString,wrapper))
   }
-  @org.junit.Test
-  def testRemoveWarapper_3() ={
+  test("testRemoveWarapper_3"){
     val wrapper:String = "\""
-    val testString = " \"te\"st\""
+    val testString = "\"te\"st\""
     val right : String = "te\"st"
-//    assertEquals( right, Parser.removeWrapper(testString,wrapper))
-    assertEquals(Parser.removeWrapper(testString,wrapper), right)
-
+    assert(right == Parser.removeWrapper(testString,wrapper))
   }
-  @org.junit.Test
-  def testsplitLine_1: Unit ={
+
+  test("testsplitLine_1"){
     val line : String =  "0002,B,2,200,2000,\"강남구,서초동\""
     val delimiter : String = ","
     val wrapper:String = "\""
 
-    println("splitline : " + Parser.splitLine(line,delimiter))
     println("parser line : " + Parser.parseLine(line, delimiter, wrapper))
+    println("splitline : " + Parser.splitLine(line,delimiter))
   }
-  @org.junit.Test
-  def testPatser_1: Unit ={
+
+  test("testPatser_1"){
     val path = "src/main/resources/test.csv"
     val line : String =  "0002,B,2,200,2000,\"강남구,서초동\""
     val delimiter : String = ","
@@ -50,7 +48,6 @@ class ParserTest {
 
     println(parser.headerValue.csvHeader)
     println(parser.bodyValue.csvBody)
-
 
   }
 }
