e51948a7689e17931c3b7272a7013c6a78a81e17
jang <ovasd12@gmail.com>
2022-04-27 23:33:41 +0900

    1. 여러 데이터 동시 parser 기능 개선 2. 동시에 출력 기능 개발 3. 출력 된 데이터에 문자열 추가기능 test

--- src/main/scala/jang/heesu/study/parser/Parser.scala
+++ src/main/scala/jang/heesu/study/parser/Parser.scala
@@ -2,9 +2,9 @@ package jang.heesu.study.parser
 
 import jang.heesu.study.csv.{CsvInfo, CsvInfoList}
 import jang.heesu.study.csv.Regular.REGULAR_SPLIT
-
-//object 키워드로 선언하는 객체는 클래스의 유일한 인스턴스를 넣기 위해 사용
-//여러 종류의 인스턴스를 받아도 처리 가능해야하므로 object 로 선언하여 사용
+/**
+ * csv 파일의 내용을 전달받은 구분자와 wrapper 를 제거하여 문자열로 만드는 기능들
+ * */
 object Parser {
   /**
    * csv 파일의 내용을 dictionary 와 비교하여 정리 된 데이터로 만들어준다
@@ -24,20 +24,38 @@ object Parser {
 
     info
   }
+  /**
+   * parsing 할 데이터를 라인별로 구분자와 wrapper 를 제거하여 문자열 만드는 기능
+   * @param line  paring 할 데이터
+   * @param delimiter 구분자
+   * @param wrapper 감싸는 wrapper
+   * */
   def parseLine(line:String, delimiter:String, wrapper:String ): List[String] ={
     splitLine(line,delimiter).map(cell => removeWrapper(cell, wrapper))
   }
-
+  /**
+   * 구분자로 데이터를 나누는 기능
+   * @param line  나눌 데이터
+   * @param delimiter 구분자
+   * */
   def splitLine(line:String, delimiter:String) : List[String] ={
     val regex = delimiter + REGULAR_SPLIT
     line.split(regex, -1).toList
   }
-
+  /**
+   * wrapper 로 감싸져있는 데이터에서 wrapper 제거하는 기능
+   * @param cell  wrapper 로 감싸져있는 데이터
+   * @param wrapper warpper 문자열
+   * */
   def removeWrapper(cell:String, wrapper:String) : String={
     if (isWrapped(cell, wrapper)) cell.trim.substring(1,cell.length-1)
     else cell
   }
-
+  /**
+   * wrapper 만 제거하기 위한 조건문
+   * @param cell wrapper 로 감싸져있는 데이터
+   * @param wrapper wrapper 문자열
+   * */
   def isWrapped(cell:String, wrapper:String) : Boolean ={
     cell.head.toString.equals(wrapper) && cell.last.toString.equals(wrapper)
   }
